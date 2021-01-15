4c76b823a584d5a872341968163d9d64c2d8cc8f
jinyong-suh <jinyong.suh@navercorp.com>
2021-01-15 14:46:26 +0900

    ㄴㅇㄹㅁㄴㅇㄹ

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -14,7 +14,9 @@ const SecondPage = () => (
     <a href="https://apps.apple.com/kr/app/%EB%84%A4%EC%9D%B4%EB%B2%84-%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-naver-cloud/id585173084">
       앱스토어로 이동
     </a>
-    <a href="navermybox://moment?version=22">앱으로 열기</a>
+    <a href="intent://moment?version=22#Intent;scheme=navermybox;action=android.intent.action.VIEW;category=android.intent.category.BROWSABLE;package=com.nhn.android.ndrive;end">
+      앱으로 열기 안드로이드
+    </a>
     <a href="navermybox://moment?version=22">앱으로 열기</a>
   </Layout>
 );
