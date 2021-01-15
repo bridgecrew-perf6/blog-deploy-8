0993a0c0556a480d944844d760087b61bad33598
jinyong-suh <jinyong.suh@navercorp.com>
2021-01-15 14:32:16 +0900

    update

--- src/pages/components/listItem.tsx
+++ src/pages/components/listItem.tsx
@@ -20,6 +20,12 @@ const ListItem: FC<Props> = ({ title, content, pageName }) => (
     >
       <h2>{title}</h2>
       <small>{content}</small>
+      <a href="navermybox://moment?version=22">앱으로 열기</a>
+      <a href="https://apps.apple.com/kr/app/%EB%84%A4%EC%9D%B4%EB%B2%84-%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-naver-cloud/id585173084">
+        앱스토어로 이동
+      </a>
+      <a href="navermybox://moment?version=22">앱으로 열기</a>
+      <a href="navermybox://moment?version=22">앱으로 열기</a>
     </div>
   </Link>
 );
