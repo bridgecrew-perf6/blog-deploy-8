907cf5aebcacbd2dddc7b61812efce0d3af9abbf
jinyong-suh <jinyong.suh@navercorp.com>
2021-01-19 13:38:15 +0900

    테스트

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -4,21 +4,25 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const Test = () => (
-  <Layout>
-    <SEO title="테스트" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+const Test = () => {
+  const handleClick = () => {
+    window.location.href =
+      "intent://moment?version=22#Intent;scheme=navermybox;action=android.intent.action.VIEW;category=android.intent.category.BROWSABLE;package=com.nhn.android.ndrive;end";
+  };
+  return (
+    <Layout>
+      <SEO title="테스트" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
 
-    <a href="navermybox://moment?version=22">앱으로 열기</a>
-    <a href="https://apps.apple.com/kr/app/%EB%84%A4%EC%9D%B4%EB%B2%84-%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-naver-cloud/id585173084">
-      앱스토어로 이동
-    </a>
-    <a href="intent://moment?version=22#Intent;scheme=navermybox;action=android.intent.action.VIEW;category=android.intent.category.BROWSABLE;package=com.nhn.android.ndrive;end">
-      앱으로 열기 안드로이드
-    </a>
-  </Layout>
-);
+      <a href="navermybox://moment?version=22">앱으로 열기</a>
+      <a href="https://apps.apple.com/kr/app/%EB%84%A4%EC%9D%B4%EB%B2%84-%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-naver-cloud/id585173084">
+        앱스토어로 이동
+      </a>
+      <a onClick={handleClick}>앱으로 열기 안드로이드</a>
+    </Layout>
+  );
+};
 
 export default Test;
